0c9ef0835597a1b6adc7c580542db96af5280fda
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-10-17 13:28:31 -0200

    Using CSS to Style Valid and Invalid Controls

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,10 +2,19 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    styles: [
+        "div { margin: 30px; }",
+        ".ng-invalid.ng-touched:not(:focus) { border: 3px solid red; }",
+        ".ng-valid:not(:focus) { border: 3px solid green; }"
+    ],
+    template: `
+        <div>
+            <input type="text"[(ngModel)]="message" required>
+        </div>
+    `,
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public message: string = "";
 
 }
