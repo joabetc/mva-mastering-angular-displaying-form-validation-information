8ab0cd605cc4abec1588249dcea350a3a3fe0c97
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-10-19 13:14:20 -0200

    Using Template References with Template Forms

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,27 +2,25 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    styles: [
-        "div { margin: 30px; }",
-        ".ng-invalid.ng-touched:not(:focus) { border: 3px solid red; }",
-        ".ng-valid:not(:focus) { border: 3px solid green; }",
-        ".ng-untouched ~ span, :focus ~ span { display: none; }",
-        ".ng-valid ~ span { display: none; }",
-        "strong { float: left; color: red; }",
-        ".ng-valid + strong, .ng-untouched ~ strong, :focus ~ strong { display: none; }",
-    ],
     template: `
-        <div>
-            <input type="text"[(ngModel)]="message" required>
-            <strong>*</strong>
-            <span>
-                Message is required.
-            </span>
-        </div>
+        <form #contactForm="ngForm" id="contact-form" novalidate>
+            <div>Submitted: {{contactForm.submitted}}</div>
+            <fieldset #addressGroup="ngModelGroup" ngModelGroup="address">
+                <div>Address Group Valid: {{addressGroup.valid}}</div>
+                <div>
+                    <strong *ngIf="streetInput.invalid && streetInput.touched">*</strong>
+                    <input type="text" #streetInput="ngModel" [(ngModel)]="street" name="streetInput" required>
+                    <span *ngIf="streetInput.invalid && streetInput.touched">
+                        Street is required.
+                    </span>
+                </div>
+            </fieldset>
+            <button type="submit">Submit</button>
+        </form>
     `,
 })
 export class AppComponent {
 
-    public message: string = "";
+    public street: string = "";
 
 }
