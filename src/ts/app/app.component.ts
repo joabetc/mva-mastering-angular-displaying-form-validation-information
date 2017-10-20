c949cecbd97cf4be994c134b40efd7e70a4d8441
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-10-20 13:18:25 -0200

    Using View Child with Template Forms

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, AfterViewInit, ViewChildren, QueryList } from "@angular/core";
+import { NgForm, NgModelGroup, NgModel } from "@angular/forms";
 
 @Component({
     selector: "main",
     template: `
-        <form #contactForm="ngForm" id="contact-form" novalidate>
+        <form id="contact-form" novalidate>
             <div>Submitted: {{contactForm.submitted}}</div>
             <fieldset #addressGroup="ngModelGroup" ngModelGroup="address">
                 <div>Address Group Valid: {{addressGroup.valid}}</div>
                 <div>
                     <strong *ngIf="streetInput.invalid && streetInput.touched">*</strong>
-                    <input type="text" #streetInput="ngModel" [(ngModel)]="street" name="streetInput" required>
+                    <label for="street-input">Street: </label>
+                    <input type="text" id="street-input" #streetInput="ngModel"
+                        [(ngModel)]="street" name="streetInput" required>
                     <span *ngIf="streetInput.invalid && streetInput.touched">
                         Street is required.
+                    </span>
+                </div>
+                <div>
+                    <strong *ngIf="cityInput.invalid && cityInput.touched">*</strong>
+                    <label for="city-input">City: </label>
+                    <input type="text" id="city-input" #cityInput="ngModel"
+                        [(ngModel)]="city" name="cityInput" required>
+                    <span *ngIf="cityInput.invalid && cityInput.touched">
+                        City is required.
                     </span>
                 </div>
             </fieldset>
@@ -19,8 +31,19 @@ import { Component } from "@angular/core";
         </form>
     `,
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
     public street: string = "";
+
+    @ViewChild(NgForm)
+    public contactForm: NgForm;
+
+    @ViewChildren(NgModel)
+    public contactControls: QueryList<NgModel>;
+
+    public ngAfterViewInit() {
+        console.log(this.contactForm);
+        console.log(this.contactControls);
+    }
 
 }
