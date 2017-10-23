0638d0e02a504945c8bc3516fa84eada0d42e686
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-10-23 13:15:40 -0200

    Using the Form Control Object tree with Reactive Forms

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,28 +1,32 @@
-import { Component, ViewChild, AfterViewInit, ViewChildren, QueryList } from "@angular/core";
-import { NgForm, NgModelGroup, NgModel } from "@angular/forms";
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: "main",
     template: `
-        <form id="contact-form" novalidate>
+        <form ngNoForm id="contact-form" novalidate [formGroup]="contactForm">
             <div>Submitted: {{contactForm.submitted}}</div>
-            <fieldset #addressGroup="ngModelGroup" ngModelGroup="address">
-                <div>Address Group Valid: {{addressGroup.valid}}</div>
+            <fieldset formGroupName="addressGroup">
+                <div>Address Group Valid: {{contactForm.controls.addressGroup.valid}}</div>
                 <div>
-                    <strong *ngIf="streetInput.invalid && streetInput.touched">*</strong>
+                    <strong *ngIf="addressGroup.controls.streetInput.invalid
+                        && addressGroup.controls.streetInput.touched">*</strong>
                     <label for="street-input">Street: </label>
-                    <input type="text" id="street-input" #streetInput="ngModel"
-                        [(ngModel)]="street" name="streetInput" required>
-                    <span *ngIf="streetInput.invalid && streetInput.touched">
+                    <input type="text" id="street-input"
+                        name="streetInput" formControlName="streetInput">
+                    <span *ngIf="addressGroup.controls.streetInput.invalid
+                        && addressGroup.controls.streetInput.touched">
                         Street is required.
                     </span>
                 </div>
                 <div>
-                    <strong *ngIf="cityInput.invalid && cityInput.touched">*</strong>
+                    <strong *ngIf="addressGroup.controls.cityInput.invalid
+                        && addressGroup.controls.cityInput.touched">*</strong>
                     <label for="city-input">City: </label>
-                    <input type="text" id="city-input" #cityInput="ngModel"
-                        [(ngModel)]="city" name="cityInput" required>
-                    <span *ngIf="cityInput.invalid && cityInput.touched">
+                    <input type="text" id="city-input"
+                        name="cityInput" formControlName="cityInput">
+                    <span *ngIf="addressGroup.controls.cityInput.invalid
+                        && addressGroup.controls.cityInput.touched">
                         City is required.
                     </span>
                 </div>
@@ -31,19 +35,15 @@ import { NgForm, NgModelGroup, NgModel } from "@angular/forms";
         </form>
     `,
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
 
-    public street: string = "";
+    public addressGroup = new FormGroup({
+        streetInput: new FormControl("", Validators.required),
+        cityInput: new FormControl("", Validators.required),
+    });
 
-    @ViewChild(NgForm)
-    public contactForm: NgForm;
-
-    @ViewChildren(NgModel)
-    public contactControls: QueryList<NgModel>;
-
-    public ngAfterViewInit() {
-        console.log(this.contactForm);
-        console.log(this.contactControls);
-    }
+    public contactForm = new FormGroup({
+        addressGroup: this.addressGroup,
+    });
 
 }
