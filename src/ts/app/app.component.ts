3557a7a43c4ece4fdd2b7ac89acae6395e5ddfa3
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-10-24 13:24:30 -0200

    Collecting Errors from Many Controls and Displaying a Summary

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,49 +1,80 @@
-import { Component } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component, ViewChild, AfterViewInit, ViewChildren, QueryList } from "@angular/core";
+import { NgForm, NgModelGroup, NgModel, FormGroup } from "@angular/forms";
 
 @Component({
     selector: "main",
     template: `
-        <form ngNoForm id="contact-form" novalidate [formGroup]="contactForm">
+        <form id="contact-form" novalidate>
             <div>Submitted: {{contactForm.submitted}}</div>
-            <fieldset formGroupName="addressGroup">
-                <div>Address Group Valid: {{contactForm.controls.addressGroup.valid}}</div>
-                <div>
-                    <strong *ngIf="addressGroup.controls.streetInput.invalid
-                        && addressGroup.controls.streetInput.touched">*</strong>
-                    <label for="street-input">Street: </label>
-                    <input type="text" id="street-input"
-                        name="streetInput" formControlName="streetInput">
-                    <span *ngIf="addressGroup.controls.streetInput.invalid
-                        && addressGroup.controls.streetInput.touched">
-                        Street is required.
-                    </span>
-                </div>
-                <div>
-                    <strong *ngIf="addressGroup.controls.cityInput.invalid
-                        && addressGroup.controls.cityInput.touched">*</strong>
-                    <label for="city-input">City: </label>
-                    <input type="text" id="city-input"
-                        name="cityInput" formControlName="cityInput">
-                    <span *ngIf="addressGroup.controls.cityInput.invalid
-                        && addressGroup.controls.cityInput.touched">
-                        City is required.
-                    </span>
-                </div>
+            <fieldset #addressGroup="ngModelGroup" ngModelGroup="address">
+                <legend>Address</legend>
+                <details *ngIf="addressGroup.invalid">
+                    <summary>
+                        Address is Invalid
+                        <small>(open for more details)</small>
+                    </summary>
+                    <ul>
+                        <ng-template ngFor let-error [ngForOf]="getGroupErrors(addressGroup)">
+                            <li *ngIf="error == 'streetInput:required'">Street is a required field.</li>
+                            <li *ngIf="error == 'cityInput:required'">City is a required field.</li>
+                        </ng-template>
+                    </ul>
+                </details>
+                <section>
+                    <div>
+                        <strong *ngIf="streetInput.invalid && streetInput.touched">*</strong>
+                        <label for="street-input">Street: </label>
+                        <input type="text" id="street-input" #streetInput="ngModel"
+                            [(ngModel)]="street" name="streetInput" required>
+                        <span *ngIf="streetInput.invalid && streetInput.touched">
+                            Street is required.
+                        </span>
+                    </div>
+                    <div>
+                        <strong *ngIf="cityInput.invalid && cityInput.touched">*</strong>
+                        <label for="city-input">City: </label>
+                        <input type="text" id="city-input" #cityInput="ngModel"
+                            [(ngModel)]="city" name="cityInput" required>
+                        <span *ngIf="cityInput.invalid && cityInput.touched">
+                            City is required.
+                        </span>
+                    </div>
+                </section>
             </fieldset>
             <button type="submit">Submit</button>
         </form>
     `,
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-    public addressGroup = new FormGroup({
-        streetInput: new FormControl("", Validators.required),
-        cityInput: new FormControl("", Validators.required),
-    });
+    public street: string = "";
 
-    public contactForm = new FormGroup({
-        addressGroup: this.addressGroup,
-    });
+    @ViewChild(NgForm)
+    public contactForm: NgForm;
+
+    @ViewChildren(NgModel)
+    public contactControls: QueryList<NgModel>;
+
+    public ngAfterViewInit() {
+        console.log(this.contactForm);
+        console.log(this.contactControls);
+    }
+
+    public getGroupErrors(fg: NgModelGroup | FormGroup) {
+
+        if (!fg) { return []; }
+
+        if (fg instanceof NgModelGroup && !fg.control) {
+            return [];
+        }
+
+        const controls = fg instanceof NgModelGroup ? fg.control.controls : fg.controls;
+
+        return [].concat(Object.keys(controls)
+            .filter((controlKey) => controls[controlKey].errors)
+            .map((controlKey) => [controlKey, Object.keys(controls[controlKey].errors)])
+            .map(([controlKey, errorKeys]: [string, string[]]) =>
+                errorKeys.map((errorKey) => controlKey + ":" + errorKey)));
+    }
 
 }
